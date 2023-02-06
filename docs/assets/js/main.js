5da8bdc3cb84f365292354f4008a2ba211c8f37b
Vladimir <ryzhovvladimirr@gmail.com>
2023-02-06 18:46:41 +0200

    add section hero with adaptive

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -8,4 +8,23 @@ var swiper = new Swiper(".hero__swiper", {
     disableOnInteraction: false,
   },
 });
+var benefitsSwiper = new Swiper(".benefits__swiper", {
+  centeredSlides: true,
+  effect: "slide",
+  loop: true,
+  grabCursor: true,
+  autoHeight: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  navigation: {
+    nextEl: ".benefits__btn-next",
+    prevEl: ".benefits__btn-prev",
+  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+});
 console.log(2)
